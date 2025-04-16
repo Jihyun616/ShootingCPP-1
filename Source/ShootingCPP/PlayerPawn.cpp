3f7ea6ea98bcb017faf23e5b304494013c8fc8ae
GGumBug <119824540+GGumBug@users.noreply.github.com>
2025-04-16 17:32:35 +0900

    플레이어 박스컴포넌트 매쉬컴포넌트 추가

---
+++ Source/ShootingCPP/PlayerPawn.cpp
@@ -0,0 +1,43 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerPawn.h"
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+APlayerPawn::APlayerPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 오브젝트의 충돌을 담당하는 컴포넌트(기능)
+	BoxComp = CreateDefaultSubobject<UBoxComponent>(TEXT("My Box Component"));
+	SetRootComponent(BoxComp);
+	BoxComp->SetBoxExtent(FVector(50.0f, 50.0f, 50.0f));
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("My Static Mesh"));
+	MeshComp->SetupAttachment(BoxComp);
+}
+
+// Called when the game starts or when spawned
+void APlayerPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlayerPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/ShootingCPP/PlayerPawn.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlayerPawn.generated.h"
+
+UCLASS()
+class SHOOTINGCPP_API APlayerPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+public:
+	// 전방 선언
+	// 이 뒤에 타입이 클래스 타입이란 걸 알려줬기 때문에
+	// 컴파일러는 class라고 인지하고 통과시켜준다.
+	// 그래서 왜 전방 선언을 하느냐, 의존성이 끊어진다.
+	// 의존성이 끊어진다는건 PlayerPawn 클래스와 UBoxComponent 클래스가
+	// 관계가 없어진다라고 표현할 수 있다.
+	UPROPERTY(EditAnywhere)
+	class UBoxComponent* BoxComp;
+
+	// 메시 컴포넌트
+	// 오브젝트에 그래픽 렌더링을 할 수 있게하는 기능
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* MeshComp;
+};
